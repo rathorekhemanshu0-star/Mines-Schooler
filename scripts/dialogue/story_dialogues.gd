fc3acf25a25a2dbb25eab4ac3f061f4e899b762f
rathorekhemanshu0-star <rathorekhemanshu0@gmail.com>
2026-05-11 11:17:25 +0530

    Create story_dialogues.gd

---
+++ scripts/dialogue/story_dialogues.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var intro_dialogue = [
+
+	"Welcome to Aether Academy...",
+	"This school feels strange...",
+	"Who is that girl on the rooftop?",
+	"My life is about to change..."
+]
